1fca4222c3eb4bb0625cc0c16ab71c3334185bf7
Neeraj Auchar <neerajauchar@gmail.com>
2023-07-04 08:57:53 +0530

    added course

---
+++ Learn-neeraj/Learn-neeraj/src/main/java/com/Neeraj/Learnneeraj/LearnNeerajApplication.java
@@ -0,0 +1,13 @@
+package com.Neeraj.Learnneeraj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnNeerajApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnNeerajApplication.class, args);
+	}
+
+}
---
+++ Learn-neeraj/Learn-neeraj/src/test/java/com/Neeraj/Learnneeraj/LearnNeerajApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Neeraj.Learnneeraj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnNeerajApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
